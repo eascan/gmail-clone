c8727fe467be06759675a8da54f5222d169df92f
Edward Ascan <edward.ascan@me.com>
2021-05-11 20:49:24 -0400

    add sections and left icons to header

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,21 @@
 import "./Header.css";
 import MenuIcon from "@material-ui/icons/Menu";
+import {IconButton} from "@material-ui/core";
 
 function Header() {
   return (
     <div className="header">
-      <h1>This is header</h1>
-      <div className="header__left"></div>
+      <div className="header__left">
+        <IconButton>
+          <MenuIcon />
+        </IconButton>
+        <img
+          src="https://i.pinimg.com/originals/ae/47/fa/ae47fa9a8fd263aa364018517020552d.png"
+          alt=""
+        />
+      </div>
+      <div className="header__middle"></div>
+      <div className="header__right"></div>
     </div>
   );
 }
