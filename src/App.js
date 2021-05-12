e381ce8fbd1a61f24c09c2c348b8c183e4616594
Edward Ascan <edward.ascan@me.com>
2021-05-11 20:43:17 -0400

    create header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 
 import "./App.css";
+import Header from "./Header";
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
+      <Header />
+
       <h2> new project</h2>
     </div>
   );
---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import "./Header.css";
+import MenuIcon from "@material-ui/icons/Menu";
+
+function Header() {
+  return (
+    <div className="header">
+      <h1>This is header</h1>
+      <div className="header__left"></div>
+    </div>
+  );
+}
+
+export default Header;
