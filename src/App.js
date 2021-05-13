525bedf16ea95c28c23d9ed8369a6e5a05590318
Edward Ascan <edward.ascan@me.com>
2021-05-12 22:33:50 -0400

    install and import react router

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import "./App.css";
 import Header from "./Header";
 import Sidebar from "./Sidebar";
 
+import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
+
 function App() {
   return (
     <div className="app">
