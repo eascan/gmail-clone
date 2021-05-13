fe23482cd8e11dbb25be787ef8818b5216d2babc
Edward Ascan <edward.ascan@me.com>
2021-05-12 22:31:15 -0400

    add footer

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,4 +1,4 @@
-import {Button} from "@material-ui/core";
+import {Button, IconButton} from "@material-ui/core";
 import AddIcon from "@material-ui/icons/Add";
 import InboxIcon from "@material-ui/icons/Inbox";
 import StarIcon from "@material-ui/icons/Star";
@@ -7,6 +7,9 @@ import LabelImportantIcon from "@material-ui/icons/LabelImportant";
 import NearMeIcon from "@material-ui/icons/NearMe";
 import NoteIcon from "@material-ui/icons/Note";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import PersonIcon from "@material-ui/icons/Person";
+import DuoIcon from "@material-ui/icons/Duo";
+import PhoneIcon from "@material-ui/icons/Phone";
 
 import "./Sidebar.css";
 import SidebarOption from "./SidebarOption";
@@ -33,6 +36,20 @@ function Sidebar() {
       <SidebarOption Icon={NearMeIcon} number={9} title="Starred" />
       <SidebarOption Icon={NoteIcon} number={28} title="Drafts" />
       <SidebarOption Icon={ExpandMoreIcon} number={28} title="More" />
+
+      <div className="sidebar__footer">
+        <div className="sidebar__footerIcons">
+          <IconButton>
+            <PersonIcon />
+          </IconButton>
+          <IconButton>
+            <DuoIcon />
+          </IconButton>
+          <IconButton>
+            <PhoneIcon />
+          </IconButton>
+        </div>
+      </div>
     </div>
   );
 }
