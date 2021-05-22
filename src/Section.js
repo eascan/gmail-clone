25b59d3a4e489cb93bee8babf9fde8372bbabcb2
Edward Ascan <edward.ascan@me.com>
2021-05-22 19:20:23 -0400

    add class conditional to section

--- src/Section.js
+++ src/Section.js
@@ -1,7 +1,11 @@
 import "./Section.css";
 
 function Section({Icon, title, color, selected}) {
-  return <div className={`section ${selected && "section--selected"}`}></div>;
+  return <div className={`section ${selected && "section--selected"}`}
+  style={{
+    borderBottom: `3px solid ${color}`,
+    color: 
+  }}></div>;
 }
 
 export default Section;
