334a124262bd2d63c636194bec06083a74d9cda0
Edward Ascan <edward.ascan@me.com>
2021-05-25 17:31:09 -0400

    finish sendmail component, add initial styling

--- src/SendMail.js
+++ src/SendMail.js
@@ -1,9 +1,25 @@
 import "./SendMail.css";
 
+import CloseIcon from "@material-ui/icons/Close";
+import {Button} from "@material-ui/core";
+
 function SendMail() {
   return (
     <div className="sendMail">
-      <div className="sendMail__header"></div>
+      <div className="sendMail__header">
+        <h3>New message</h3>
+        <CloseIcon className="sendMail__close" />
+      </div>
+
+      <form>
+        <input type="text" />
+        <input type="text" />
+        <input type="text" />
+
+        <div className="sendMail__options">
+          <Button>Send</Button>
+        </div>
+      </form>
     </div>
   );
 }
