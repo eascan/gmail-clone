2f599af4905e71c1094cd40d89984ef1efc84c78
Edward Ascan <edward.ascan@me.com>
2021-05-25 17:38:23 -0400

    add most of the styling

--- src/SendMail.js
+++ src/SendMail.js
@@ -12,12 +12,23 @@ function SendMail() {
       </div>
 
       <form>
-        <input type="text" />
-        <input type="text" />
-        <input type="text" />
+        <input placeholder="To" type="text" />
+        <input placeholder="Subject" type="text" />
+        <input
+          placeholder="Message..."
+          type="text"
+          className="sendMail__message"
+        />
 
         <div className="sendMail__options">
-          <Button>Send</Button>
+          <Button
+            className="sendMail__send"
+            variant="contained"
+            color="primary"
+            type="submit"
+          >
+            Send
+          </Button>
         </div>
       </form>
     </div>
