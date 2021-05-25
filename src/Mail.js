eb9a095ebeaa4a6b056174fa4f7c8db7ebb41264
Edward Ascan <edward.ascan@me.com>
2021-05-25 16:37:40 -0400

    add redux mailSlice store layer

--- src/Mail.js
+++ src/Mail.js
@@ -8,16 +8,22 @@ import EmailIcon from "@material-ui/icons/Email";
 import WatchLaterIcon from "@material-ui/icons/WatchLater";
 import CheckCircleIcon from "@material-ui/icons/CheckCircle";
 import LabelImportantIcon from "@material-ui/icons/LabelImportant";
+import UnfoldMoreIcon from "@material-ui/icons/UnfoldMore";
+import PrintIcon from "@material-ui/icons/Print";
+import ExitToAppIcon from "@material-ui/icons/ExitToApp";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
 import {IconButton} from "@material-ui/core";
+import {useHistory} from "react-router-dom";
 
 function Mail() {
+  const history = useHistory();
+
   return (
     <div className="mail">
       <div className="mail">
         <div className="mail__tools">
           <div className="mail__toolsLeft">
-            <IconButton>
+            <IconButton onClick={() => history.push("/")}>
               <ArrowBackIcon />
             </IconButton>
 
@@ -54,7 +60,32 @@ function Mail() {
             </IconButton>
           </div>
 
-          <div className="mail__toolsRight"></div>
+          <div className="mail__toolsRight">
+            <IconButton>
+              <UnfoldMoreIcon />
+            </IconButton>
+
+            <IconButton>
+              <PrintIcon />
+            </IconButton>
+
+            <IconButton>
+              <ExitToAppIcon />
+            </IconButton>
+          </div>
+        </div>
+      </div>
+
+      <div className="mail__body">
+        <div className="mail__bodyHeader">
+          <h2>Subject</h2>
+          <LabelImportantIcon className="mail__important" />
+          <p>Title</p>
+          <p className="mail__time">10pm</p>
+        </div>
+
+        <div className="mail__message">
+          <p>This is a message</p>
         </div>
       </div>
     </div>
