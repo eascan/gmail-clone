827bb986da6c1e44f841f358cd4ae90ef1d7cae5
Edward Ascan <edward.ascan@me.com>
2021-05-25 15:18:57 -0400

    add usehistory to email component, and start to layout mail page

--- src/Mail.js
+++ src/Mail.js
@@ -1,7 +1,64 @@
 import "./Mail.css";
 
+import ArrowBackIcon from "@material-ui/icons/ArrowBack";
+import MoveToInboxIcon from "@material-ui/icons/MoveToInbox";
+import ErrorIcon from "@material-ui/icons/Error";
+import DeleteIcon from "@material-ui/icons/Delete";
+import EmailIcon from "@material-ui/icons/Email";
+import WatchLaterIcon from "@material-ui/icons/WatchLater";
+import CheckCircleIcon from "@material-ui/icons/CheckCircle";
+import LabelImportantIcon from "@material-ui/icons/LabelImportant";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import {IconButton} from "@material-ui/core";
+
 function Mail() {
-  return <div className="mail"></div>;
+  return (
+    <div className="mail">
+      <div className="mail">
+        <div className="mail__tools">
+          <div className="mail__toolsLeft">
+            <IconButton>
+              <ArrowBackIcon />
+            </IconButton>
+
+            <IconButton>
+              <MoveToInboxIcon />
+            </IconButton>
+
+            <IconButton>
+              <ErrorIcon />
+            </IconButton>
+
+            <IconButton>
+              <DeleteIcon />
+            </IconButton>
+
+            <IconButton>
+              <EmailIcon />
+            </IconButton>
+
+            <IconButton>
+              <WatchLaterIcon />
+            </IconButton>
+
+            <IconButton>
+              <CheckCircleIcon />
+            </IconButton>
+
+            <IconButton>
+              <LabelImportantIcon />
+            </IconButton>
+
+            <IconButton>
+              <MoreVertIcon />
+            </IconButton>
+          </div>
+
+          <div className="mail__toolsRight"></div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Mail;
