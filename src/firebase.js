81120b8f2b9160c3f654656e54b17313dd52426c
Edward Ascan <edward.ascan@me.com>
2021-05-25 22:11:48 -0400

    set up firebase store

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAlc-RpaEslNSlDdZwHB5udyAuot3O-mQk",
+  authDomain: "clone-289d7.firebaseapp.com",
+  projectId: "clone-289d7",
+  storageBucket: "clone-289d7.appspot.com",
+  messagingSenderId: "554848358575",
+  appId: "1:554848358575:web:004bbeb70e6318d879445a",
+  measurementId: "G-CBSTCTR104",
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
